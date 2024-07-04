79c40ba7c5290f0d40a3a03164e119766ee06546
Vladimir <vladimir.rostok96@gmail.com>
2024-07-04 06:23:56 +0300

    Push a backbone of DDD, the domain restrictions (state validator)

---
+++ src/domain/word/model.go
@@ -0,0 +1,26 @@
+package word
+
+import (
+	"fmt"
+	domain_errors "test-assignment/domain/errors"
+	"unicode"
+)
+
+// Verify the word, only a full string of unicode letters is allowed.
+func IsValidUnicode(s string) error {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return fmt.Errorf("error: %w", domain_errors.InvalidWordData{})
+		}
+	}
+	return nil
+}
+
+// Verify the word, only a certain length is allowed.
+func IsValidLength(s string, length int) error {
+	if len(s) != length {
+		return fmt.Errorf("error: %w", domain_errors.InvalidWordLength{})
+	}
+
+	return nil
+}
